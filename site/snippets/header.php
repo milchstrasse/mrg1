9f56ebc4b372d14537ba540ffebba9640a588dee
Marc René Geiser <Marc Ren Geiser>
2016-10-17 17:53:44 +0200

    scss.php hinzugefügt

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <?php snippet('scss') ?>
+    
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
